725aef46d9b8f542bdadae68aa683c341991208d
Alina Skrypets <alinaskripets92@gmail.com>
2024-05-23 14:32:32 +0200

    remove contact page

--- src/pages/contact.tsx
+++
@@ -1,8 +0,0 @@
-import { NextPage } from "next";
-import ContactUs from "../components/screens/ContactUs/ContactUs";
-
-const Contact: NextPage = () => {
-  return <ContactUs />;
-};
-
-export default Contact;
